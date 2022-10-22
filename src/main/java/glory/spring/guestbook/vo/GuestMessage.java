4ff58fdf95aa39325fd55ae178378aec3e0cec31
twozeropang <pma5758@naver.com>
2022-10-22 13:39:41 +0900

    table information update

--- src/main/java/glory/spring/guestbook/vo/GuestMessage.java
+++ src/main/java/glory/spring/guestbook/vo/GuestMessage.java
@@ -2,6 +2,25 @@ package glory.spring.guestbook.vo;
 
 import java.util.Date;
 
+/*
+ * GUESTBOOK TABLE
+
+CREATE TABLE GUESTBOOK (
+MESSAGE_ID NUMBER(5,0), 
+GUEST_NAME VARCHAR2(20) DEFAULT null, 
+MESSAGE VARCHAR2(4000), 
+REGISTRY_DATE DATE DEFAULT null, 
+CONSTRAINT GUESTBOOK_PK PRIMARY KEY (MESSAGE_ID) 
+);
+
+
+SEQUENCE 생성 
+
+CREATE SEQUENCE GUEST_SEQ MINVALUE 1 MAXVALUE 99999 
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE;
+
+ */
+
 public class GuestMessage {
 	private Integer id;
 	private String guestName;
